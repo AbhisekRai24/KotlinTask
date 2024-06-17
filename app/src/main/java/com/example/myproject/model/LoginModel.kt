9b8e815dfdab5b8dbee4cefa8c5409a0d3209cad
ahbisek <abhisek1999rai@gmail.com>
2024-06-17 23:15:18 +0545

    tried prodcut model

--- app/src/main/java/com/example/myproject/model/LoginModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myproject.model
-
-data class LoginModel(
-    var email : String = "",
-    var password : String = ""
-) {
-}
--- app/src/main/java/com/example/myproject/repository/LoginRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myproject.repository
-
-import com.example.myproject.model.LoginModel
-
-interface LoginRepository {
-    fun addEmail(loginModel: LoginModel, callback :(Boolean, String?) -> Unit)
-    fun addPassword(loginModel: LoginModel,callback : (Boolean,String?)-> Unit)
-}
